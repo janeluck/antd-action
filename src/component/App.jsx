2772d2c3bd7f3b5db5e34b46be19493df3f1e0f7
janeluck <janeluck158@gmail.com>
2016-06-18 19:52:22 +0800

    InputNumber Style

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import {Timeline, Input } from 'antd';
+import {Timeline, Input, Select, InputNumber } from 'antd';
 import './App.less';
 import reqwest from 'reqwest'
 
+
+import  { Component } from 'react';
+const Option = Select.Option;
+
+function handleChange(value) {
+    console.log(`selected ${value}`);
+}
 export default class App extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            historyItems: []
-        }
+
     }
+
     componentDidMount() {
         const _this = this
         reqwest({
@@ -33,34 +39,22 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Timeline>
+            <div style={{margin: 30}}>
 
-                    {this.state.historyItems.map((item, i) => {
+                <div className="jane-input-number">
+                    <div className="jane-input-number-handle-wrap">
+                        <a href="javascript:;" className="jane-input-number-handler jane-input-number-handler-up">
+                            <span className="jane-input-number-handler-up-inner"></span>
+                        </a>
+                        <a href="javascript:;" className="jane-input-number-handler jane-input-number-handler-down">
+                            <span className="jane-input-number-handler-down-inner"></span>
+                        </a>
 
-                        return (<Timeline.Item color="green" key={i}>{item.title + '  ' + item.description + '   ' + item.date}</Timeline.Item>)
-                    })}
+                    </div>
+                    <div className="jane-input-number-input-wrap">
+                        <input type="text" className="jane-input-number-input"/>
+                    </div>
 
-                    <Timeline.Item color="green">创建服务现场 2015-09-01</Timeline.Item>
-                    <Timeline.Item color="green">创建服务现场 2015-09-01</Timeline.Item>
-                    <Timeline.Item color="red">
-                        <p>初步排除网络异常1</p>
-                        <p>初步排除网络异常2</p>
-                        <p>初步排除网络异常3 2015-09-01</p>
-                    </Timeline.Item>
-                    <Timeline.Item>
-                        <p>技术测试异常1</p>
-                        <p>技术测试异常2</p>
-                        <p>技术测试异常3 2015-09-01</p>
-                    </Timeline.Item>
-                </Timeline>
-
-
-
-
-                <div className="ant-input-wrapper">
-
-                    <input type="number" className="ant-input"/>
                 </div>
             </div>)
 
