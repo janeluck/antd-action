af325a0cdba40442761a99f1af3660cdf8c5882f
janeluck <janeluck158@gmail.com>
2016-07-08 13:34:10 +0800

    TreeDemo

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Timeline, Input, Select, InputNumber, Popover, Button,Tree } from 'antd';
+import {Timeline, Input, Select, InputNumber, Popover, Button} from 'antd';
 
 import '../jane/EditDialog/css/basic_new_v2.css';
 import '../jane/EditDialog/css/pagev2.3.css';
@@ -7,11 +7,12 @@ import './App.less';
 import reqwest from 'reqwest'
 import JanePopup from '../jane/JanePopup'
 import EditDialog from '../jane/EditDialog'
+import TreeDemo from '../jane/DoubleTree'
 
 import  { Component } from 'react';
 const Option = Select.Option;
 
-const TreeNode = Tree.TreeNode;
+
 const style = {
     padding: '.3em .8em',
     border: '1px solid rgba(0, 0, 0, .1)',
@@ -191,91 +192,6 @@ class EditDialog_Demo extends Component {
 
 
 
-const x = 3;
-const y = 2;
-const z = 1;
-const gData = [];
-
-const generateData = (_level, _preKey, _tns) => {
-    const preKey = _preKey || '0';
-    const tns = _tns || gData;
-
-    const children = [];
-    for (let i = 0; i < x; i++) {
-        const key = `${preKey}-${i}`;
-        tns.push({ title: key, key });
-        if (i < y) {
-            children.push(key);
-        }
-    }
-    if (_level < 0) {
-        return tns;
-    }
-    const level = _level - 1;
-    children.forEach((key, index) => {
-        tns[index].children = [];
-        return generateData(level, key, tns[index].children);
-    });
-};
-generateData(z);
-
-
-const Tree_Demo = React.createClass({
-    getDefaultProps() {
-        return {
-            multiple: true,
-        };
-    },
-    getInitialState() {
-        return {
-            expandedKeys: ['0-0-0', '0-0-1'],
-            autoExpandParent: true,
-            checkedKeys: ['0-0-0'],
-            selectedKeys: [],
-        };
-    },
-    onExpand(expandedKeys) {
-        console.log('onExpand', arguments);
-        // if not set autoExpandParent to false, if children expanded, parent can not collapse.
-        // or, you can remove all expanded chilren keys.
-        this.setState({
-            expandedKeys,
-            autoExpandParent: false,
-        });
-    },
-    onCheck(checkedKeys) {
-        this.setState({
-            checkedKeys,
-            selectedKeys: ['0-3', '0-4'],
-        });
-    },
-    onSelect(selectedKeys, info) {
-        console.log('onSelect', info);
-        this.setState({ selectedKeys });
-    },
-    render() {
-        const loop = data => data.map((item) => {
-            if (item.children) {
-                return (
-                    <TreeNode key={item.key} title={item.key} disableCheckbox={item.key === '0-0-0'}>
-                        {loop(item.children)}
-                    </TreeNode>
-                );
-            }
-            return <TreeNode key={item.key} title={item.key} />;
-        });
-        return (
-            <Tree checkable multiple={this.props.multiple}
-                  onExpand={this.onExpand} expandedKeys={this.state.expandedKeys}
-                  autoExpandParent={this.state.autoExpandParent}
-                  onCheck={this.onCheck} checkedKeys={this.state.checkedKeys}
-                  onSelect={this.onSelect} selectedKeys={this.state.selectedKeys}
-            >
-                {loop(gData)}
-            </Tree>
-        );
-    },
-});
 
 
 export default class App extends React.Component {
@@ -366,7 +282,7 @@ export default class App extends React.Component {
                 </div>
                 <EditDialog_Demo/>
 
-                <Tree_Demo />
+                <TreeDemo />
 
             </div>)
 
