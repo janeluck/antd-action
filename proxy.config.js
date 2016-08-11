24c09121ef7228f3806f310d7e3b05c90f7dedcb
janeluck <janeluck158@gmail.com>
2016-08-11 14:56:50 +0800

    Mock

--- proxy.config.js
+++ proxy.config.js
@@ -1,7 +1,13 @@
 const Mock = require('mockjs')
 module.exports = {
     'GET /api/users': {data: [1, 2], msg: 'success'},
-    'POST /api/upload': {rs: true},
+    'POST /api/upload': Mock.mock({
+        rs: true,
+        "data|1-10000": 100
+
+    }),
+
+
     'GET /api/admin': Mock.mock({
         success: true,
         data: {name: '@Name'},
